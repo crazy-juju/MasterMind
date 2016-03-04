ad4b009a205ff9bf83e0d4c63db70112d7e807be
julien <julien@192.168.1.14>
2016-03-04 21:23:34 +0100

    Init Model

---
+++ src/Model/Couleurs.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author julien
+ */
+public class Couleurs {
+    public String label;
+    public int r,v,b;
+
+    public Couleurs(String label, int r, int v, int b) {
+        this.label = label;
+        this.r = r;
+        this.v = v;
+        this.b = b;
+    }
+    
+    
+}
---
+++ src/Model/Grid.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import Model.Square;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author julien
+ */
+public class Grid {
+    ArrayList<Square> cases = new ArrayList<>();
+}
